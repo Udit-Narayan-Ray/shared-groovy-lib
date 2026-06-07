56661793811dcb10648a28a527fa8d6012f56214
Udit Narayan Ray <91251041+Udit-Narayan-Ray@users.noreply.github.com>
2026-06-08 01:17:35 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,5 +1,5 @@
 def call(String dockerHubUser, String imageName, String imageTag){
-    withCredentails([usernamePassword(credentailsId: "DockerHubCred", usernameVariable: "dockerHubUser", passwordVariable: "dockerHubPass")]){
+    withCredentials([usernamePassword(credentialsId: "DockerHubCred", usernameVariable: "dockerHubUser", passwordVariable: "dockerHubPass")]){
       sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
     }
     sh "docker push ${dockerHubUser}/${imageName}:${imageTag}"
